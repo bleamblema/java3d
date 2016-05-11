3b012eaffe0fb880b554b95fc78c32daefc6c4aa
bleamblema <bleamblema@gmail.com>
2016-05-11 19:13:51 +0700

    Optimizing

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -1,6 +1,9 @@
 package engineTester;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 import models.RawModel;
 import models.TexturedModel;
@@ -10,9 +13,8 @@ import org.lwjgl.util.vector.Vector3f;
 
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
+import renderEngine.MasterRenderer;
 import renderEngine.OBJLoader;
-import renderEngine.Renderer;
-import shaders.StaticShader;
 import textures.ModelTexture;
 import entities.Camera;
 import entities.Entity;
@@ -25,15 +27,13 @@ public class MainGameLoop {
 		DisplayManager.createDisplay();
 		
 		Loader loader = new Loader();
-		StaticShader shader = new StaticShader();
-		Renderer renderer = new Renderer(shader);
 		
-		RawModel model = OBJLoader.loadObjModel("dragon", loader);
+		RawModel model = OBJLoader.loadObjModel("box", loader);
 		
 		//RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
 		int textureID = 0;
 		try {
-			textureID = loader.loadTexture("white");
+			textureID = loader.loadTexture("image");
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(-1);
@@ -46,24 +46,36 @@ public class MainGameLoop {
 		texture.setReflectivity(1);
 		TexturedModel staticModel = new TexturedModel(model, texture);
 		
-		Entity entity = new Entity(staticModel, new Vector3f(0,0,-50), 0, 0, 0, 1);
+//		Entity entity = new Entity(staticModel, new Vector3f(0,0,-50), 0, 0, 0, 1);
 		Light light = new Light(new Vector3f(0, 0, -20), new Vector3f(1,1,1));
 		
 		Camera camera = new Camera();
 		
+		Random random = new Random();
+		List<Entity> allCubes = new ArrayList<Entity>();
+		
+		for(int i = 0; i< 200; i++){
+			float x = random.nextFloat() * 100 - 50;
+			float y = random.nextFloat() * 100 - 50;
+			float z = random.nextFloat() * -300;
+			allCubes.add(new Entity(staticModel, new Vector3f(x,y,z), random.nextFloat() * 180f, 
+					random.nextFloat() * 180f, 0f, 1f));
+			
+ 		}
+		
+		MasterRenderer renderer = new MasterRenderer();
 		while(!Display.isCloseRequested()){
-			entity.increaseRotation(0, 1, 0);
-			camera.move();
-			renderer.prepare();
-			shader.start();
-			shader.loadLight(light);
-			shader.loadViewMatrix(camera);
-			renderer.render(entity, shader);
-			shader.stop();
+			
+			for(Entity cube : allCubes){
+				renderer.precessEntity(cube);
+			}
+			
+			
+			renderer.render(light, camera);
 			DisplayManager.updateDisplay();
 		}
 		
-		shader.cleanUp();
+		renderer.cleanUp();
 		loader.cleanUp();
 		DisplayManager.closeDisplay();
 
