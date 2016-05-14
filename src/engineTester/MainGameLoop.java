1eef8670dd37912a8392bd48b4c9b3805b918d90
bleamblema <bleamblema@gmail.com>
2016-05-14 20:18:07 +0700

    Soft Edges

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -123,7 +123,7 @@ public class MainGameLoop {
 			float distance = 2 * (camera.getPosition().y-waters.get(0).getHeight());
 			camera.getPosition().y -= distance;
 			camera.invertPitch();
-			renderer.renderScene(entities, terrains, lights, camera, new Vector4f(0, 1, 0, -waters.get(0).getHeight()));
+			renderer.renderScene(entities, terrains, lights, camera, new Vector4f(0, 1, 0, -waters.get(0).getHeight()+1f));
 			camera.getPosition().y += distance;
 			camera.invertPitch();
 			
--- src/water/WaterShader.java
+++ src/water/WaterShader.java
@@ -23,6 +23,7 @@ public class WaterShader extends ShaderProgram {
 	private int location_normalMap;
 	private int location_lightColour;
 	private int location_lightPosition;
+	private int location_depthMap;
 
 	public WaterShader() {
 		super(VERTEX_FILE, FRAGMENT_FILE);
@@ -46,6 +47,7 @@ public class WaterShader extends ShaderProgram {
 		location_normalMap = getUniformLocation("normalMap");
 		location_lightColour = getUniformLocation("lightColour");
 		location_lightPosition = getUniformLocation("lightPosition");
+		location_depthMap = getUniformLocation("depthMap");
 	}
 	
 	public void loadLight(Light sun){
@@ -62,6 +64,7 @@ public class WaterShader extends ShaderProgram {
 		super.loadInt(location_refractionTexture, 1);
 		super.loadInt(location_dudvMap, 2);
 		super.loadInt(location_normalMap, 3);
+		super.loadInt(location_depthMap, 4);
 	}
 
 	public void loadProjectionMatrix(Matrix4f projection) {
